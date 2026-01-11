4625dfb8760a3e17755b6c398f21c33f93f2c73d
Alan <alankemalov@gmail.com>
2026-01-11 20:24:31 +0300

    fix: clean routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const { Room } = require('../models/db.js');
-var indexRouter = require('./routes/index');
-app.use('/', indexRouter);
-
 
 // 🛡️ Middleware авторизации
 function requireAuth(req, res, next) {
